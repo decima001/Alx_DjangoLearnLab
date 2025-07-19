a6343eb2093c1b98c6b731566ddac1300ef32fdf
Ifedapo Rasaq <arasaq040@gmail.com>
2025-07-19 19:43:54 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,6 +13,21 @@ class Book(models.Model):
 
     class Meta:
         permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+        
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    library = models.ForeignKey('Library', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
             ("can_borrow_book", "Can borrow book"),
             ("can_reserve_book", "Can reserve book"),
         ]
