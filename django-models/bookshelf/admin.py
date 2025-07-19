91023302269977bea857e86edb45e97cd1fc48e5
Rasaq Ifedapo <decimalimited@gmail.com>
2025-07-19 12:38:14 +0100

    Fixed relationship_app setup and model issues

--- django-models/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-# Register the Book model with custom admin configurations
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Show these fields in the list view
-    list_filter = ('publication_year', 'author')  # Add filters for easy navigation
-    search_fields = ('title', 'author')  # Enable search by title and author
