37e628830c7660b3acf0371d974be4c25fe0a0a6
Rasaq Ifedapo <decimalimited@gmail.com>
2025-07-19 11:55:20 +0100

    Restructure: Rename project to LibraryProject and add relationship_app models

--- django-models/LibraryProject/wsgi.py
+++ django-models/LibraryProject/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from LibraryProject.settings import ...
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RelationshipProject.settings')
 
