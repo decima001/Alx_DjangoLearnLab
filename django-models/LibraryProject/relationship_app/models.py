34960b9e4a466c4d8795e69b6c427f4cc53a03a9
Ifedapo Rasaq <arasaq040@gmail.com>
2025-07-19 15:47:02 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,22 +1,22 @@
-# Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.role}"
 
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    library = models.ForeignKey('Library', on_delete=models.CASCADE)
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
