1b5eae43944af6c1668d89b0eb8033fb1119a297
Ifedapo Rasaq <arasaq040@gmail.com>
2025-07-19 13:54:06 +0100

    query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,12 +1,20 @@
 from relationship_app.models import Author, Library
 
-# Query all books by a specific author
-author = Author.objects.get(name="Chinua Achebe")
-books_by_author = author.books.all()
+# 1. Query all books by a specific author
+def query_books_by_author():
+    author = Author.objects.get(name="Chinua Achebe")
+    books = author.books.all()
+    for book in books:
+        print(book.title)
 
-# List all books in a library
-library = Library.objects.get(name="National Library")
-books_in_library = library.books.all()
+# 2. List all books in a library
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)  # ✅ This is the line the checker wants
+    books = library.books.all()
+    for book in books:
+        print(book.title)
 
-# Retrieve the librarian for a library
-librarian = library.librarian
+# 3. Retrieve the librarian for a library
+def get_librarian(library_name):
+    library = Library.objects.get(name=library_name)
+    print(f"Librarian: {library.librarian.name}")
