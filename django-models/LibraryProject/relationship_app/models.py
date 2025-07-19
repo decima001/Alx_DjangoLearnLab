cca06d300f0b927f5965adaa001e9b77e14aacec
Ifedapo Rasaq <arasaq040@gmail.com>
2025-07-19 19:42:43 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,47 +1,35 @@
-# Create your models here.
-
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    library = models.ForeignKey('Library', on_delete=models.CASCADE)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance, role='Member')  # Default role
+    def __str__(self):
+        return self.title
 
-
+    class Meta:
+        permissions = [
+            ("can_borrow_book", "Can borrow book"),
+            ("can_reserve_book", "Can reserve book"),
+        ]
 class UserProfile(models.Model):
-    ROLE_CHOICES = [
+    ROLE_CHOICES = (
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
-    ]
-    
+    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    library = models.ForeignKey('Library', on_delete=models.CASCADE)
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
