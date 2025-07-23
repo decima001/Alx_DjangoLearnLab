8c95bf5c27753991725a560cdd27f75ca33d29ed
Ifedapo Rasaq <arasaq040@gmail.com>
2025-07-23 22:52:22 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,92 +1,27 @@
-# Create your models here.
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-# UserProfile Model
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
-    
-    def is_admin(self):
-        """Check if user has Admin role"""
-        return self.role == 'Admin'
-    
-    def is_librarian(self):
-        """Check if user has Librarian role"""
-        return self.role == 'Librarian'
-    
-    def is_member(self):
-        """Check if user has Member role"""
-        return self.role == 'Member'
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
 
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-# Automatically create UserProfile when a User is created
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance, role='Member')  # Default role
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    if hasattr(instance, 'userprofile'):
-        instance.userprofile.save()
-    else:
-        UserProfile.objects.create(user=instance)
-
-
-# Author Model
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.name
-
-
-# Library Model
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-# Book Model
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    library = models.ForeignKey(Library, on_delete=models.CASCADE)
-    publication_year = models.IntegerField()
-    
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        
-        ]
-    
-    def __str__(self):
-        return self.title
-
-
-
-# Librarian Model
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+        return self.username
