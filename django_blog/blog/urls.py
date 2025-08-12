bfc2cb4831b9057cfac70d7b3a24db84e20deea5
Rasaq Ifedapo <decimalimited@gmail.com>
2025-08-12 15:47:45 +0100

    Updated urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,21 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import register_view, profile_view
 
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', register_view, name='register'),
